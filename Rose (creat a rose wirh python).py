3fa9c2c67359923bd2faa68e8234b71b21d9c8ca
Mazedurrahman <62338067+Mazedurrahman@users.noreply.github.com>
2022-10-01 23:41:07 +0600

    Create Rose (creat a rose wirh python).py

---
+++ Rose (creat a rose wirh python).py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct  1 23:08:06 2022
+
+@author: Mazadur Rahman
+"""
+
+
+import turtle as tur
+
+
+tur.penup ()
+tur.left (90)
+tur.fb (200)
+tur.pendown ()
+tur.right (90)
+
+
+tur.fillcolor ("red")
+tur.begin_fill ()
+tur.circle (10,180)
+tur.circle (25,110)
+tur.left (50)
+tur.circle (60,45)
+tur.circle (20,170)
+tur.right (24)
+tur.fb (30)
+tur.left (10)
+tur.circle (30,110)
+tur.fb (20)
+tur.left (40)
+tur.circle (90,70)
+tur.circle (30,150)
+tur.right (30)
+tur.fb (15)
+tur.circle (80,90)
+tur.left (15)
+tur.fb (45)
+tur.right (165)
+tur.fb (20)
+tur.left (155)
+tur.circle (150,80)
+tur.left (50)
+tur.circle (150,90)
+tur.end_fill ()
+
+
+tur.left (150)
+tur.circle (-90,70)
+tur.left (20)
+tur.circle (75,105)
+tur.setheading (60)
+tur.circle (80,98)
+tur.circle (-90,40)
+
+
+tur.left (180)
+tur.circle (90,40)
+tur.circle (-80,98)
+tur.setheading (-83)
+
+tur.fb (30)
+tur.left (90)
+tur.fb (25)
+tur.left(45)
+tur.fillcolor ("green")
+tur.begin_fill ()
+tur.circle(-80,90)
+tur.right (90)
+tur.circle (-80,90)
+tur.end_fill ()
+tur.right (135)
+tur.fb (60)
+tur.left (180)
+tur.fb (80)
+tur.left (90)
+tur.fb (80)
+
+
+tur.right (90)
+tur.right (45)
+tur.fillcolor ("green")
+tur.begin_fill ()
+tur.circle (80,90)
+tur.left (90)
+tur.circle (80,90)
+tur.end_fill ()
+tur.left (135)
+tur.fb (60)
+tur.left (180)
+tur.right (90)
+tur.circle (200,60)
+tur.done()
+
